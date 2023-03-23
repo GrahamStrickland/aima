c7f81d3fe7643f246564638ed527c2a9220e6e51
Graham Strickland <grahamhstrickland@gmail.com>
2023-03-23 14:24:49 +0200

    Finish Simple-Reflex-Agent and test vacuum_world_score.py

--- ch02/agents/simple_reflex_agent.py
+++ ch02/agents/simple_reflex_agent.py
@@ -7,25 +7,30 @@ from modules.sensor import Sensor
 class SimpleReflexAgent:
     def __init__(self):
         self.rules = {'Dirty': 'Suck',
-                      'A': 'Right',
-                      'B': 'Left'}
+                      'Clean': {
+                          'A': 'Right',
+                          'B': 'Left'
+                          }
+                      }
 
     @staticmethod
     def _interpret_input(percept: Sensor) -> Environment:
-        state = {percept.name: percept.value}
+        return {percept.name: percept.value}
 
-        return Environment('Square', state)
+    def _rule_match(self, state: dict[str, str]) -> Actuator:
+        action: str = ''
 
-    def _rule_match(self, state: Environment) -> Actuator:
-        if state.state == 'Dirty':
-            action = self.rules[state.state]
-        else:
-            action = self.rules[state.name]
+        for k, v in state.items():
+            if v == 'Dirty':
+                action = self.rules[v]
+            else:
+                action = self.rules['Clean'][k]
+            break
 
         return Actuator(name='action', value=action)
 
     def get_action(self, percept: Sensor) -> Actuator:
-        state: Environment = self._interpret_input(percept)
+        state: dict[str, str] = self._interpret_input(percept)
         rule: Actuator = self._rule_match(state)
         action: str = rule.value
 
