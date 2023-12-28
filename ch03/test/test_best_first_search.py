ccbfa6d06ed5535e38256265857cac410c2247d7
GrahamStrickland <grahamhstrickland@gmail.com>
2023-12-28 12:32:10 +0200

    Add test directory and stub test for Best-First-Search

---
+++ ch03/test/test_best_first_search.py
@@ -0,0 +1,13 @@
+#!/usr/env/bin python3
+"""Tests for the Best-First-Search algorithm."""
+
+from ..algorithms import best_first_search
+
+
+def f(node: Node) -> float: 
+    return node.path_cost
+
+
+def test_best_first_search() -> None:
+    pass
+
