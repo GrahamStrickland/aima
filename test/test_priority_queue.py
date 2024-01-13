91c291f88a315fcb39a17f9b9fbf291bac800fed
GrahamStrickland <grahamhstrickland@gmail.com>
2024-01-13 11:36:11 +0200

    Fix test class for priority queue and add test for pop()

--- test/test_priority_queue.py
+++ test/test_priority_queue.py
@@ -7,12 +7,9 @@ def f(node: Node) -> float:
     return node.path_cost
 
 
-class PriorityQueueTest:
+class TestPriorityQueue:
     """Test suite for PriorityQueue data structure."""
-    _queue: PriorityQueue
-
-    def __init__(self):
-        self._queue = PriorityQueue(f)
+    _queue = PriorityQueue(f)
 
 
     def test_init(self) -> None:
@@ -24,6 +21,16 @@ class PriorityQueueTest:
         self._queue.add(Node(state={}, parent=None, action="None", path_cost=0))
 
         assert not self._queue.is_empty()
+
+        self._queue.pop()
+
+        assert self._queue.is_empty()
+
+    def test_pop(self) -> None:
+        A = Node(state={}, parent=None, action="None", path_cost=0)
+        self._queue.add(A)
+
+        assert self._queue.top() == A
 
         self._queue.pop()
 
