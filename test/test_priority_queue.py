2f6ef08a696fbff285c6cccff355010f5f141d69
GrahamStrickland <grahamhstrickland@gmail.com>
2024-01-13 11:45:56 +0200

    Add remaining tests for priority queue

--- test/test_priority_queue.py
+++ test/test_priority_queue.py
@@ -11,7 +11,6 @@ class TestPriorityQueue:
     """Test suite for PriorityQueue data structure."""
     _queue = PriorityQueue(f)
 
-
     def test_init(self) -> None:
         assert self._queue.is_empty()
 
@@ -35,4 +34,39 @@ class TestPriorityQueue:
         self._queue.pop()
 
         assert self._queue.is_empty()
+
+    def test_top(self) -> None: 
+        assert self._queue.top() == None
+
+        A = Node(state={}, parent=None, action="None", path_cost=0)
+        self._queue.add(A)
+
+        assert self._queue.top() == A
+
+        B = Node(state={}, parent=None, action="None", path_cost=1)
+        self._queue.add(B)
+
+        assert self._queue.top() == A
+
+    def test_add(self) -> None:
+        while not self._queue.is_empty():
+            node = self._queue.pop()
+
+        A = Node(state={}, parent=None, action="None", path_cost=0)
+        B = Node(state={}, parent=None, action="None", path_cost=1)
+        C = Node(state={}, parent=None, action="None", path_cost=2)
+
+        assert self._queue.top() == None
+
+        self._queue.add(C)
+
+        assert self._queue.top() == C
+
+        self._queue.add(A)
+
+        assert self._queue.top() == A
+
+        self._queue.add(B)
+
+        assert self._queue.top() == A
 
