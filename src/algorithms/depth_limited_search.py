be177a8b815725060ec29b69ac2d43a67c7f64e9
Graham Strickland <grahamhstrickland@gmail.com>
2024-02-24 10:00:30 +0200

    Add Depth-Limited-Search and tests

--- src/algorithms/depth_limited_search.py
+++ src/algorithms/depth_limited_search.py
@@ -3,6 +3,7 @@
 
 from queue import LifoQueue
 
+from .expand import expand
 from ..data_structures import Node, Problem
 
 
@@ -15,7 +16,13 @@ def depth(node: Node) -> int:
     Returns:
         depth (int): The depth of the node in the tree.
     """
-    pass
+    depth = 0
+    
+    while node.parent is not None: 
+        depth += 1
+        node = node.parent
+
+    return depth
 
 
 def is_cycle(node: Node) -> bool:
@@ -29,10 +36,18 @@ def is_cycle(node: Node) -> bool:
         is_cycle (bool): True if the path to the given node contains 
                          a cycle, else false.
     """
-    pass
+    ancestor = node.parent
 
+    while ancestor is not None:
+        if ancestor.state == node.state:
+            return True
+        else:
+            ancestor = ancestor.parent
+    
+    return False
+    
 
-def depth_limited_search(problem: Problem) -> Node | None:
+def depth_limited_search(problem: Problem, l: int) -> Node | str | None:
     """Expands the root node, the next child, repeating this process
     until the depth limit is reached, then backtracking to the next
     successor of the root. This process is repeated until a goal is 
@@ -40,8 +55,25 @@ def depth_limited_search(problem: Problem) -> Node | None:
 
     Args:
         problem: A problem instance.
+        l: Depth limit for the search.
 
     Returns:
         A solution node or failure (None).
     """
-    pass
+    frontier: LifoQueue[Node] = LifoQueue()
+    frontier.put(Node(state=problem.initial(), path_cost=0))
+    result = None
+
+    while not frontier.empty():
+        node = frontier.get()
+
+        if problem.is_goal(node.state):
+            return node
+
+        if depth(node) > l:
+            result = "cutoff" 
+        elif not is_cycle(node):
+            for child in expand(problem, node):
+                frontier.put(child)
+        
+    return result
--- test/algorithms/test_depth_limited_search.py
+++ test/algorithms/test_depth_limited_search.py
@@ -6,14 +6,22 @@ from src.data_structures import Node, Problem
 
 
 class TestDepthLimitedSearch:
-    def test_depth(node: Node) -> None:
-        pass
+    def test_depth(self, nodes: list[Node]) -> None:
+        assert depth(nodes[-1]) == 6
+        assert depth(nodes[-2]) == 6
+        assert depth(nodes[-3]) == 6
+        assert depth(nodes[-4]) == 5
+        assert depth(nodes[-5]) == 5
 
-    def test_is_cycle(node: Node) -> None:
-        pass
+    def test_is_cycle(self, nodes: list[Node]) -> None:
+        assert not is_cycle(nodes[-1])
+        assert not is_cycle(nodes[-2])
+        assert not is_cycle(nodes[-3])
+        assert not is_cycle(nodes[-4])
+        assert not is_cycle(nodes[-5])
 
-    def test_depth_limited_search(problem: Problem) -> None:
-        solution = depth_limited_search(problem)
+    def test_depth_limited_search(self, problem: Problem) -> None:
+        solution = depth_limited_search(problem, 9)
 
         assert solution.state == "Bucharest"
         assert solution.parent.state == "Fagaras"
